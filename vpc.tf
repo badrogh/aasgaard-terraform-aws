b608df21df1724b444e0ce046ee21de1d249f426
Fab Viguier <badrogh@gmail.com>
2020-11-24 14:00:22 +0000

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -25,7 +25,7 @@ resource "aws_internet_gateway" "vpc_ig" {
 resource "aws_subnet" "vpc_public_sn" {
   vpc_id = aws_vpc.vpc_name.id
   cidr_block = var.vpc_public_subnet_1_cidr
-  availability_zone = lookup(var.availability_zone, var.vpc_region)
+  availability_zone = lookup(var.vpc_availability_zone, var.vpc_region)
   tags = {
     Name = "${var.vpc_name}-public-sn"
   }
@@ -35,7 +35,7 @@ resource "aws_subnet" "vpc_public_sn" {
 resource "aws_subnet" "vpc_private_sn" {
   vpc_id = aws_vpc.vpc_name.id
   cidr_block = var.vpc_private_subnet_1_cidr
-  availability_zone = lookup(var.availability_zone, var.vpc_region)
+  availability_zone = lookup(var.vpc_availability_zone, var.vpc_region)
   tags = {
     Name = "${var.vpc_name}-private-sn"
   }
