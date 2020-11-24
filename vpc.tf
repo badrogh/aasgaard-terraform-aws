1908dc0ea1cfdea86c159cb56bfdf4dd84a8b444
Fab Viguier <badrogh@gmail.com>
2020-11-24 14:01:52 +0000

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -46,7 +46,7 @@ resource "aws_route_table" "vpc_public_sn_rt" {
   vpc_id = aws_vpc.vpc_name.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.demo_ig.id
+    gateway_id = aws_internet_gateway.vpc_ig.id
   }
   tags = {
     Name = "${var.vpc_name}-rt"
