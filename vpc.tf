b7bd0355dbebeb88c8fce610f0af97520cbead8d
Fab Viguier <badrogh@gmail.com>
2020-11-24 11:48:31 +0000

    Update code to Terraform 0.13 syntax

--- vpc.tf
+++ vpc.tf
@@ -1,21 +1,21 @@
 # Setup our aws provider
 provider "aws" {
-  access_key = "${var.AWS_ACCESS_KEY_ID}"
-  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
-  region = "${var.vpc_region}"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region = var.vpc_region
 }
 
 # Define a vpc
 resource "aws_vpc" "vpc_name" {
-  cidr_block = "${var.vpc_cidr_block}"
+  cidr_block = var.vpc_cidr_block
   tags {
-    Name = "${var.vpc_name}"
+    Name = var.vpc_name
   }
 }
 
 # Internet gateway for the public subnet
 resource "aws_internet_gateway" "demo_ig" {
-  vpc_id = "${aws_vpc.vpc_name.id}"
+  vpc_id = aws_vpc.vpc_name.id
   tags {
     Name = "demo_ig"
   }
@@ -23,9 +23,9 @@ resource "aws_internet_gateway" "demo_ig" {
 
 # Public subnet
 resource "aws_subnet" "vpc_public_sn" {
-  vpc_id = "${aws_vpc.vpc_name.id}"
-  cidr_block = "${var.vpc_public_subnet_1_cidr}"
-  availability_zone = "${lookup(var.availability_zone, var.vpc_region)}"
+  vpc_id = aws_vpc.vpc_name.id
+  cidr_block = var.vpc_public_subnet_1_cidr
+  availability_zone = lookup(var.availability_zone, var.vpc_region)
   tags {
     Name = "vpc_public_sn"
   }
@@ -33,8 +33,8 @@ resource "aws_subnet" "vpc_public_sn" {
 
 # Private subnet
 resource "aws_subnet" "vpc_private_sn" {
-  vpc_id = "${aws_vpc.vpc_name.id}"
-  cidr_block = "${var.vpc_private_subnet_1_cidr}"
+  vpc_id = aws_vpc.vpc_name.id
+  cidr_block = var.vpc_private_subnet_1_cidr
   availability_zone = "eu-west-2b"
   tags {
     Name = "vpc_private_sn"
@@ -43,10 +43,10 @@ resource "aws_subnet" "vpc_private_sn" {
 
 # Routing table for public subnet
 resource "aws_route_table" "vpc_public_sn_rt" {
-  vpc_id = "${aws_vpc.vpc_name.id}"
+  vpc_id = aws_vpc.vpc_name.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.demo_ig.id}"
+    gateway_id = aws_internet_gateway.demo_ig.id
   }
   tags {
     Name = "vpc_public_sn_rt"
@@ -55,22 +55,22 @@ resource "aws_route_table" "vpc_public_sn_rt" {
 
 # Associate the routing table to public subnet
 resource "aws_route_table_association" "vpc_public_sn_rt_assn" {
-  subnet_id = "${aws_subnet.vpc_public_sn.id}"
-  route_table_id = "${aws_route_table.vpc_public_sn_rt.id}"
+  subnet_id = aws_subnet.vpc_public_sn.id
+  route_table_id = aws_route_table.vpc_public_sn_rt.id
 }
 
 # ECS Instance Security group
 resource "aws_security_group" "vpc_public_sg" {
   name = "demo_pubic_sg"
   description = "demo public access security group"
-  vpc_id = "${aws_vpc.vpc_name.id}"
+  vpc_id = aws_vpc.vpc_name.id
 
   ingress {
     from_port = 22
     to_port = 22
     protocol = "tcp"
     cidr_blocks = [
-      "${var.vpc_access_from_ip_range}"]
+      var.vpc_access_from_ip_range]
   }
 
   ingress {
@@ -78,7 +78,7 @@ resource "aws_security_group" "vpc_public_sg" {
     to_port = 0
     protocol = "tcp"
     cidr_blocks = [
-      "${var.vpc_public_subnet_1_cidr}"]
+      var.vpc_public_subnet_1_cidr]
   }
 
   egress {
@@ -97,7 +97,7 @@ resource "aws_security_group" "vpc_public_sg" {
 resource "aws_security_group" "vpc_private_sg" {
   name = "demo_private_sg"
   description = "demo security group to access private ports"
-  vpc_id = "${aws_vpc.vpc_name.id}"
+  vpc_id = aws_vpc.vpc_name.id
 
   # allow memcached port within VPC
   ingress {
@@ -105,7 +105,7 @@ resource "aws_security_group" "vpc_private_sg" {
     to_port = 11211
     protocol = "tcp"
     cidr_blocks = [
-      "${var.vpc_public_subnet_1_cidr}"]
+      var.vpc_public_subnet_1_cidr]
   }
 
   # allow redis port within VPC
@@ -114,7 +114,7 @@ resource "aws_security_group" "vpc_private_sg" {
     to_port = 6379
     protocol = "tcp"
     cidr_blocks = [
-      "${var.vpc_public_subnet_1_cidr}"]
+      var.vpc_public_subnet_1_cidr]
   }
 
   # allow postgres port within VPC
@@ -123,7 +123,7 @@ resource "aws_security_group" "vpc_private_sg" {
     to_port = 5432
     protocol = "tcp"
     cidr_blocks = [
-      "${var.vpc_public_subnet_1_cidr}"]
+      var.vpc_public_subnet_1_cidr]
   }
 
   # allow mysql port within VPC
@@ -132,7 +132,7 @@ resource "aws_security_group" "vpc_private_sg" {
     to_port = 3306
     protocol = "tcp"
     cidr_blocks = [
-      "${var.vpc_public_subnet_1_cidr}"]
+      var.vpc_public_subnet_1_cidr]
   }
 
   egress {
@@ -148,25 +148,25 @@ resource "aws_security_group" "vpc_private_sg" {
 }
 
 output "vpc_region" {
-  value = "${var.vpc_region}"
+  value = var.vpc_region
 }
 
 output "vpc_id" {
-  value = "${aws_vpc.vpc_name.id}"
+  value = aws_vpc.vpc_name.id
 }
 
 output "vpc_public_sn_id" {
-  value = "${aws_subnet.vpc_public_sn.id}"
+  value = aws_subnet.vpc_public_sn.id
 }
 
 output "vpc_private_sn_id" {
-  value = "${aws_subnet.vpc_private_sn.id}"
+  value = aws_subnet.vpc_private_sn.id
 }
 
 output "vpc_public_sg_id" {
-  value = "${aws_security_group.vpc_public_sg.id}"
+  value = aws_security_group.vpc_public_sg.id
 }
 
 output "vpc_private_sg_id" {
-  value = "${aws_security_group.vpc_private_sg.id}"
+  value = aws_security_group.vpc_private_sg.id
 }
