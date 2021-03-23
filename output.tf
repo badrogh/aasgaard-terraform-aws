4286837bf91091a35dfc3e6acafe06daf8ecf6e1
Fab Viguier <badrogh@gmail.com>
2021-03-23 14:10:57 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -7,12 +7,10 @@ output "vpc_id" {
   value = aws_vpc.vpc_name.id
 }
 
-# Section ONLY needed for using Terraform Cloud, comment out if using TerraformCLI
-# Running TerraformCLI you don't need to output private key on console which is instead wrote into output folder
-output "instance_private_key" {
-  value = tls_private_key.instance_key_pair.private_key_pem
+output "connector_ids" {
+  value = [aws_instance.centrify_connector.*.id]
 }
 
-output "instance_ids" {
-  value = [aws_instance.centrify_connector.*.id]
+output "connector_ips" {
+  value = [aws_instance.centrify_connector.*.private_ip]
 }
